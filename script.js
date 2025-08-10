f6604e82dd1da2daae9ba0c811394e3d8f59ccd6
sagario9 <saikatsardar180@gmail.com>
2025-08-10 22:33:10 +0530

    Updated Experiment 1A

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function time() {
   var m = d.getMinutes();
   var h = d.getHours();
   span.textContent = 
-    ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2) + " | " + ("0" + p).substr(-4) + "-" + ("0" + r).substr(-2) + "-" + ("0" + q).substr(-2) ;
+    ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2) + " | " + ("0" + p).substr(-2) + "-" + ("0" + r).substr(-2) + "-" + ("0" + q).substr(-2) ;
 }
 
 setInterval(time, 1000);
